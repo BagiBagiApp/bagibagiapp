1dbfb5112f46f1eed72cb825e42440beca47d2f5
Naufal Ammar Saputra <102176304+NoobersPro@users.noreply.github.com>
2024-06-10 14:19:48 +0700

    update Fragment

--- bagibagi-frontend/app/src/main/java/com/bagibagi/app/ui/main/MainActivity.kt
+++ bagibagi-frontend/app/src/main/java/com/bagibagi/app/ui/main/MainActivity.kt
@@ -1,63 +1,46 @@
 package com.bagibagi.app.ui.main
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.bagibagi.app.R
 import com.bagibagi.app.databinding.ActivityMainBinding
-//import com.bagibagi.app.ui.notifications.NotificationsActivity
-//import com.bagibagi.app.ui.add.AddActivity
-//import com.bagibagi.app.ui.history.HistoryActivity
-import com.bagibagi.app.ui.profile.ProfileActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.bagibagi.app.ui.HistoryFragment
+import com.bagibagi.app.ui.HomeFragment
+import com.bagibagi.app.ui.NotificationsFragment
+import com.bagibagi.app.ui.ProfileFragment
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity()  {
+    private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val bottomNavigation: BottomNavigationView = binding.bottomNavigation
-
-        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_home -> if (!isCurrentActivity(MainActivity::class.java)) {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    overridePendingTransition(0, 0)
-                    finish()
-                }
-//                R.id.nav_notifications -> if (!isCurrentActivity(NotificationsActivity::class.java)) {
-//                    startActivity(Intent(this, NotificationsActivity::class.java))
-//                    overridePendingTransition(0, 0)
-//                    finish()
-//                }
-//                R.id.nav_add -> if (!isCurrentActivity(AddActivity::class.java)) {
-//                    startActivity(Intent(this, AddActivity::class.java))
-//                    overridePendingTransition(0, 0)
-//                    finish()
-//                }
-//                R.id.nav_history -> if (!isCurrentActivity(HistoryActivity::class.java)) {
-//                    startActivity(Intent(this, HistoryActivity::class.java))
-//                    overridePendingTransition(0, 0)
-//                    finish()
-//                }
-                R.id.nav_profile -> if (!isCurrentActivity(ProfileActivity::class.java)) {
-                    startActivity(Intent(this, ProfileActivity::class.java))
-                    overridePendingTransition(0, 0)
-                    finish()
-                }
+                R.id.nav_home -> openFragment(HomeFragment())
+                R.id.nav_notifications -> openFragment(NotificationsFragment())
+                R.id.nav_history -> openFragment(HistoryFragment())
+                R.id.nav_profile -> openFragment(ProfileFragment())
             }
             true
         }
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
 
-        // Set default selection
-        bottomNavigation.selectedItemId = R.id.nav_home
+        binding.fabAdd.setOnClickListener {
+            Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show()
+        }
     }
-
-    private fun isCurrentActivity(activityClass: Class<out AppCompatActivity>): Boolean {
-        return this::class.java == activityClass
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
