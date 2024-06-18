8a63fe003fb250419f32c57ba293eb9db1961c9a
Wensel Alvin Viking <143996126+MilkyLemper@users.noreply.github.com>
2024-06-18 21:46:03 +0700

    [UDPATE] Create TransactionRepository

---
+++ bagibagi-frontend/app/src/main/java/com/bagibagi/app/data/repo/TransactionRepository.kt
@@ -0,0 +1,12 @@
+package com.bagibagi.app.data.repo
+
+import com.bagibagi.app.data.api.ApiService
+
+class TransactionRepository private constructor(private val  apiService : ApiService) {
+    fun getExchangeFromOther(){
+
+    }
+    fun getExchangeToOther(){
+        
+    }
+}
